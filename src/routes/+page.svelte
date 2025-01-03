98b721f2c9ce25d9e0764e4e4e23068293662848
Lukas Runge <login@lukasrunge.de>
2025-01-03 20:20:55 +0100

    feat: :children_crossing: add footer with imprint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -62,8 +62,8 @@
 		<h1 class=" text-5xl font-bold">🚂 SORRY 🛤️</h1>
 		{#if currentRemark !== undefined}
 			<div class="flex flex-col gap-4">
-				<div class="card bg-neutral text-neutral-content shadow-xl md:max-w-xl">
-					<div class="card-body"><p>{@html currentRemark.text}</p></div>
+				<div class="card max-w-sm bg-neutral text-neutral-content shadow-xl md:max-w-xl">
+					<div class="card-body"><p class="break-words">{@html currentRemark.text}</p></div>
 				</div>
 			</div>
 			<button class="btn btn-primary" onclick={() => newRemark()}>{currentReaction}</button>
