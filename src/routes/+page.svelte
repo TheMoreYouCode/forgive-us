3611fc63f60388a7eb2025a72e712d892ea799d2
Lukas Runge <login@lukasrunge.de>
2025-01-03 12:49:01 +0100

    fix: :bug: fix nested a tag not allowed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,12 +13,11 @@
 
 	<div class="flex flex-col gap-4">
 		{#each data.remarks as remark}
-			<a
-				href="#"
+			<div
 				class="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
 			>
-				<p class="font-normal text-gray-700 dark:text-gray-400">{@html remark.text}</p>
-			</a>
+				<p class="break-words font-normal text-gray-700 dark:text-gray-400">{@html remark.text}</p>
+			</div>
 		{/each}
 	</div>
 </div>
